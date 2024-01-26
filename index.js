7e9fd48b068b9eff03dba116c427f8d06b0e7fab
subbareddy <subbareddy4327@gmail.com>
2024-01-26 06:28:37 +0530

    modified localstorage issuse

--- index.js
+++ index.js
@@ -4,74 +4,76 @@ let carouselList1 = document.querySelector("#carouselList1");
 let carouselList2 = document.querySelector("#carouselList2");
 
 let searchText = document.querySelector("#searchText");
-let faviMoviesList=[]
+let faviMoviesList = [];
 let sortedFav;
 
-const sortElement=document.querySelector(".drop-down-options");
+const sortElement = document.querySelector(".drop-down-options");
 
 // sorted the faviroteMovies by relesedYear and movieName
-sortElement.addEventListener("change",(event)=>{
-      if(event.target.value==="name"){
-         sortedFav=faviMoviesList.sort((a,b)=>a.title.localeCompare(b.title)); //sorted for name
-      }else if(event.target.value==="year"){
-         sortedFav=faviMoviesList.sort((a,b)=>((parseInt((a.release_date).split("-")[0]))-(parseInt((b.release_date).split("-")[0])))) //sorted for year
-      }
-      localStorage.setItem("faviMovies",JSON.stringify(sortedFav)); //storing data into localStorage
-      faviroteSlideList()
-        
-})
-
-
+sortElement.addEventListener("change", (event) => {
+  if (event.target.value === "name") {
+    sortedFav = faviMoviesList.sort((a, b) => a.title.localeCompare(b.title)); //sorted for name
+  } else if (event.target.value === "year") {
+    sortedFav = faviMoviesList.sort(
+      (a, b) =>
+        parseInt(a.release_date.split("-")[0]) -
+        parseInt(b.release_date.split("-")[0])
+    ); //sorted for year
+  }
+  localStorage.setItem("faviMovies", JSON.stringify(sortedFav)); //storing data into localStorage
+  faviroteSlideList();
+});
 
 //faviroteMovie slide functionality
-function faviroteSlideList(){
-    carouselList2.textContent="";
-  const favList=JSON.parse(localStorage.getItem("faviMovies"))
-  faviMoviesList = favList
-    favList.forEach(async each=>{
-        const { title, poster_path, id } =each;
-        const movieDetail = await fetch(
-            `https://api.themoviedb.org/3/movie/${id}?api_key=2ad18877c2ecce5382256b80fefda964`
-          );
-          const res = await movieDetail.json();
-          
-          const { genres } = res;
-          let genereNames = "";
-          genres.forEach((each) => {
-            genereNames += each.name + " ";
-          });
+function faviroteSlideList() {
+  carouselList2.textContent = "";
+  const localdata = localStorage.getItem("faviMovies")
+  if (localdata===null){
+    faviMoviesList = []
+  }else{
+    faviMoviesList = JSON.parse(localdata)
+  }
+  faviMoviesList.forEach(async (each) => {
+    const { title, poster_path, id } = each;
+    const movieDetail = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=2ad18877c2ecce5382256b80fefda964`
+    );
+    const res = await movieDetail.json();
 
-        splide2.destroy();
-        let listItem2 = document.createElement("li");
-        listItem2.id=id;
-        listItem2.classList.add("splide__slide");
-        carouselList2.appendChild(listItem2);
+    const { genres } = res;
+    let genereNames = "";
+    genres.forEach((each) => {
+      genereNames += each.name + " ";
+    });
 
-        let heartEL = document.createElement("img");
-        heartEL.src="./images/redliked.png"
-        heartEL.classList.add("heart-icon");
-        heartEL.addEventListener("click",()=>{
-          const index=faviMoviesList.findIndex((each)=>{
-            if(each.id===id){
-              return true;
-            }else{
-              return false
-            }
-          })
-          faviMoviesList.splice(index,1);
-          localStorage.setItem("faviMovies",JSON.stringify(faviMoviesList));
+    splide2.destroy();
+    let listItem2 = document.createElement("li");
+    listItem2.id = id;
+    listItem2.classList.add("splide__slide");
+    carouselList2.appendChild(listItem2);
 
-          faviroteSlideList(faviMoviesList)
+    let heartEL = document.createElement("img");
+    heartEL.src = "./images/redliked.png";
+    heartEL.classList.add("heart-icon");
+    heartEL.addEventListener("click", () => {
+      const index = faviMoviesList.findIndex((each) => {
+        if (each.id === id) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+      faviMoviesList.splice(index, 1);
+      localStorage.setItem("faviMovies", JSON.stringify(faviMoviesList));
 
-        })
-        listItem2.appendChild(heartEL);
+      faviroteSlideList(faviMoviesList);
+    });
+    listItem2.appendChild(heartEL);
 
-
-
-        let imgEl = document.createElement("img");
-        imgEl.classList.add("carousel-img");
-        imgEl.src = `https://image.tmdb.org/t/p/w500/${poster_path}`;
-        listItem2.appendChild(imgEl);
+    let imgEl = document.createElement("img");
+    imgEl.classList.add("carousel-img");
+    imgEl.src = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+    listItem2.appendChild(imgEl);
 
     let movieTitle = document.createElement("p");
     movieTitle.classList.add("movie-title");
@@ -82,50 +84,43 @@ function faviroteSlideList(){
     genreEl.textContent = genereNames;
     listItem2.appendChild(genreEl);
     splide2.mount();
-
-    })
-    
-
+  });
 }
 
 faviroteSlideList();
 
-
 //recommended slide functionality
 function carouselList(recommendedMoviesList) {
-  carouselList1.textContent="";
+  carouselList1.textContent = "";
   recommendedMoviesList.forEach(async (Movie) => {
-   
-    const { title, poster_path, id } =
-      Movie;
+    const { title, poster_path, id } = Movie;
     const movieDetail = await fetch(
       `https://api.themoviedb.org/3/movie/${id}?api_key=2ad18877c2ecce5382256b80fefda964`
     );
     const res = await movieDetail.json();
-    
+
     const { genres } = res;
     let genereNames = "";
     genres.forEach((each) => {
       genereNames += each.name + " ";
     });
-   
+
     splide1.destroy();
     searchText.textContent = "";
     let listItem = document.createElement("li");
     listItem.classList.add("splide__slide");
     carouselList1.appendChild(listItem);
     let heartEL = document.createElement("img");
-    heartEL.src="./images/Favorite.png"
-    heartEL.addEventListener("click",()=>{
-      const faviObj=faviMoviesList.find((each)=>each.id===id)
-      
+    heartEL.src = "./images/Favorite.png";
+    heartEL.addEventListener("click", () => {
+      const faviObj = faviMoviesList.find((each) => each.id === id);
 
-      if(faviObj===undefined){
+      if (faviObj === undefined) {
         faviMoviesList.push(Movie);
       }
-      localStorage.setItem("faviMovies",JSON.stringify(faviMoviesList)); //storing data into localStorage
+      localStorage.setItem("faviMovies", JSON.stringify(faviMoviesList)); //storing data into localStorage
       faviroteSlideList();
-    })
+    });
 
     heartEL.classList.add("heart-icon");
     listItem.appendChild(heartEL);
@@ -151,13 +146,13 @@ async function renderSearchMovie(movie) {
     `https://api.themoviedb.org/3/movie/${id}?api_key=2ad18877c2ecce5382256b80fefda964`
   );
   const res = await movieDetail.json();
-  
+
   const { genres } = res;
   let genereNames = "";
   genres.forEach((each) => {
     genereNames += each.name + " ";
   });
- 
+
   const getRecommended = await fetch(
     `https://api.themoviedb.org/3/discover/movie?api_key=2ad18877c2ecce5382256b80fefda964&with_genres=${genre_ids.join(
       ","
@@ -189,7 +184,7 @@ async function getValue() {
   };
   const responseData = await fetch(apiUrl, options);
   const data = await responseData.json();
- 
+
   const { results } = data;
   const firstMovieData = results[0];
   renderSearchMovie(firstMovieData);
@@ -201,32 +196,32 @@ const fetchData = () => {
 //carousel for recommendationMovies
 let splide1 = new Splide("#carousel1", {
   type: "loop",
-  gap:"15px",
+  gap: "15px",
   perPage: 4,
-  pagination:false,
-  breakpoints:{
-    700:{
-      perPage:2
+  pagination: false,
+  breakpoints: {
+    700: {
+      perPage: 2,
     },
-    1100:{
-      perPage:3
-    }
-  }
+    1100: {
+      perPage: 3,
+    },
+  },
 });
 
 //carousel for faviroteMovies
 let splide2 = new Splide("#carousel2", {
-    gap:"15px",
-    perPage: 4,
-    pagination:false,
-    breakpoints:{
-      700:{
-        perPage:2
-      },
-      1100:{
-        perPage:3
-      }
-    }
-  });
+  gap: "15px",
+  perPage: 4,
+  pagination: false,
+  breakpoints: {
+    700: {
+      perPage: 2,
+    },
+    1100: {
+      perPage: 3,
+    },
+  },
+});
 
 fetchData();
